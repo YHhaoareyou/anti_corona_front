aa0f7f69ed9e30e7ca0f9c20ed3c385b9b71ce59
YHHaoAreYou <mlapenang1998@gmail.com>
2020-04-03 22:00:55 +0900

    Modify: users charset from utf8mb4 to to utf8

--- db/schema.rb
+++ db/schema.rb
@@ -60,7 +60,7 @@ ActiveRecord::Schema.define(version: 2020_04_02_055750) do
     t.datetime "updated_at", null: false
   end
 
-  create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4", force: :cascade do |t|
+  create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "email", default: "", limit: 200, null: false
     t.string "encrypted_password", default: "", null: false
     t.string "reset_password_token", limit: 200
